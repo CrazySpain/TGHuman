57a06e71afc79a6f4e9b109385d57455b30596cb
CrazySpain <toraholdman@gmail.com>
2015-03-10 22:56:05 +0200

    Created TGHumanStructure.

---
+++ TGHuman/TGHumanStructure.c
@@ -0,0 +1,9 @@
+//
+//  TGHumanStructure.c
+//  TGHuman
+//
+//  Created by Tora on 3/10/15.
+//  Copyright (c) 2015 Tora. All rights reserved.
+//
+
+#include "TGHumanStructure.h"
---
+++ TGHuman/TGHumanStructure.h
@@ -0,0 +1,14 @@
+//
+//  TGHumanStructure.h
+//  TGHuman
+//
+//  Created by Tora on 3/10/15.
+//  Copyright (c) 2015 Tora. All rights reserved.
+//
+
+#ifndef __TGHuman__TGHumanStructure__
+#define __TGHuman__TGHumanStructure__
+
+#include <stdio.h>
+
+#endif /* defined(__TGHuman__TGHumanStructure__) */
